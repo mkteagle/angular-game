eb11e8e610bc457332df349563ac1c2b0274bbb2
Michael Teagle <mkteagle@gmail.com>
2016-03-02 12:38:35 -0700

    updated ionic. etc

--- app/www/js/gameService.js
+++ app/www/js/gameService.js
@@ -103,26 +103,19 @@
         self.gameState = function () {
             self.user.$ref().child('gameplay').update(self.recorded);
         };
-
         function leaderboard() {
             var childData = {};
             ref.once("value", function(snapshot) {
-                // The callback function will get called twice, once for "fred" and once for "barney"
                 snapshot.forEach(function(childSnapshot) {
-                    // key will be "fred" the first time and "barney" the second time
                     var key = childSnapshot.key();
-                    // childData will be the actual contents of the child
                     childData = childSnapshot.val();
                     angular.forEach(childData, function(value) {
-                        console.log(value.gameplay);
-
                         self.leaders.push(value);
-                        console.log(self.leaders);
-
                     });
                 });
 
             });
+            $state.go('app.leaderboard');
         }
 
         function firebaseAuthLogin(provider) {
