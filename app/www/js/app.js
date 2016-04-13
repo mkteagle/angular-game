6263837a0f509e7426552ba651b283c8065818d4
newdaveeh <newdaveeh@gmail.com>
2016-04-13 17:30:15 -0600

    Getting used to coding
    Styling app.js a little how I know...

--- app/www/js/app.js
+++ app/www/js/app.js
@@ -4,8 +4,12 @@
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
 // 'starter.controllers' is found in controllers.js
+var starter = angular.module('starter',
+    ['ionic', 'app.ctrl','ionic-material', 'ionMdInput',
+    'gameController', 'ngToast', 'firebase', 'gameService',
+    'app.login', 'ngStorage', 'upgradeDirective', 'nameFilters',
+    'angular-toArrayFilter' ])
 
-angular.module('starter', ['ionic','app.ctrl', 'ionic-material', 'ionMdInput', 'gameController', 'ngToast', 'firebase', 'gameService', 'app.login', 'ngStorage', 'upgradeDirective', 'nameFilters', 'angular-toArrayFilter'])
     .constant('firebaseUrl', "https://donut-click.firebaseio.com/")
     .run(function($ionicPlatform) {
         $ionicPlatform.ready(function() {
